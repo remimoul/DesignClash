3277fa86b32a59c3d6b520c7975662b1344faba9
Rémi <115152594+remimoul@users.noreply.github.com>
2023-11-22 23:01:18 +0100

    fix css bug and add toast

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -4,13 +4,22 @@ import star from '../assets/image/star.svg';
 import check from '../assets/image/check.svg';
 import letterx from '../assets/image/letter.svg';
 import ques from '../assets/image/ques.png';
+import { MdDelete } from "react-icons/md";
+import { Toaster, toast } from 'sonner'
+import Confetti from 'react-confetti';
+import useSound from 'use-sound';
+import yaySound from '../assets/sound/clap.mp3';
+
 
 function GamePage() {
   const [players, setPlayers] = useState([]);
   const [newPlayer, setNewPlayer] = useState('');
   const [gameStarted, setGameStarted] = useState(false);
   const [highestScoringPlayer, setHighestScoringPlayer] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [showConfetti, setShowConfetti] = useState(false);
   const navigate = useNavigate();
+  const [play] = useSound(yaySound);
 
   const endGame = () => {
     setGameStarted(false);
@@ -43,8 +52,24 @@ function GamePage() {
     );
   }
   
+  
+
   const increment = (index) => {
-    setPlayers(players.map((player, idx) => idx === index ? { ...player, score: player.score + 10 } : player));
+    
+    // setPlayers(players.map((player, idx) => idx === index ? { ...player, score: player.score + 10 } : player));
+    setPlayers(players.map((player, idx) => {
+      if (idx === index) {
+        const newScore = player.score + 10;
+        if (newScore >= 100) {
+          setMessage(toast(`Player ${player.name} has reached 100 points!`));
+          setShowConfetti(true);
+          play();
+          setTimeout(() => setShowConfetti(false), 2500); // Disparaît après 2 secondes
+        }
+        return { ...player, score: newScore };
+      }
+      return player;
+    }));
   };
 
   const decrement = (index) => {
@@ -65,6 +90,7 @@ function GamePage() {
   if (gameStarted) {
     return (
       <div>
+        {showConfetti && <Confetti />}
         <div className='container-logo'>
         <img className='logo-ques' src={ques} alt='star'></img>
         <h1 className='addPlayTitle'>Design Clash</h1>
@@ -79,7 +105,7 @@ function GamePage() {
           </div>
           </div>
         ))}
-
+      {message && <Toaster richColors closeButton position="top-center" />}
         <button onClick={endGame}>End game</button>
       </div>
     );
@@ -99,18 +125,20 @@ function GamePage() {
       
       <span className='titleAdd'>Add players</span>
       <input className='addPlayer' type="text" placeholder="Player name" value={newPlayer} onChange={(e) => setNewPlayer(e.target.value)} />
-      <button className='plusPlayer' onClick={addPlayer}>+</button>
-
+      {players.length === 0 ? <button className='plusPlayer' onClick={addPlayer}>+</button> : null}
+       
       <ul className='containerPlayer'>
         {players.map((player, index) => (
           <li key={index}>
             <input className='addOnePlayer' type="text" placeholder='eeee' value={player.name}  />
-            <button onClick={() => removePlayer(index)}>-</button>
+            {index === players.length - 1 && <button className='plusPlayerLI' onClick={addPlayer}>+</button>}
+            {index === players.length - 1 && <button className='startGame' onClick={startGame} disabled={players.length === 0}>Start</button> }
+            <MdDelete style={{width:"10%",height:"0%",marginBottom:"-3%",color:"red"}} onClick={() => removePlayer(index)}>-</MdDelete>
           </li>
         ))}
       </ul>
-
-      <button className='startGame' onClick={startGame} disabled={players.length === 0}>Start</button>
+     
+      {/* <button className='startGame' onClick={startGame} disabled={players.length === 0}>Start</button> */}
       <div className='tableHome' alt='sable'></div>
       <div className='brains' alt='brains'></div>
       <div className='Question' alt='object'></div>
